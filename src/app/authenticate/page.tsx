d89ef3ce824844afece1e6e107883ffb045c44d5
qiewi <105619444+qiewi@users.noreply.github.com>
2024-12-06 14:05:03 +0700

    feat: sign up form

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import TabSwitcher from '@/src/components/TabSwitcher'
+import React from 'react'
+// import SignInForm from './SignInForm'
+// import SignUpForm from './SignUpForm'
+// import { getUser } from '@/lib/lucia'
+import { redirect } from 'next/navigation'
+import SignInForm from './SignInForm'
+// import GoogleOAuthButton from '@/src/components/GoogleOAuthButton'
 
-const AuthenticatePage = () => {
+const AuthenticatePage = async () => {
+  // const user = await getUser()
+  // if (user) {
+  //   return redirect('/dashboard')
+  // }
   return (
-    <div>
-      AuthenticatePage
+    <div className='relative flex w-full h-screen bg-background'>
+      <div className='max-w-3xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+        {/* <GoogleOAuthButton /> */}
+        <div className="h-4"></div>
+        <TabSwitcher SignInTab={<SignInForm/>} SignUpTab={<h1>Sign Up</h1>} />
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default AuthenticatePage;
+export default AuthenticatePage
