b305c64421ffedc9db210358f804f838c19daa2e
qiewi <105619444+qiewi@users.noreply.github.com>
2024-12-19 21:33:25 +0700

    feat: profile page fixed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -110,52 +110,60 @@ const ProfilePage = () => {
       <div className="min-h-screen bg-gray-50 flex flex-col items-center py-8 px-4">
         <div className="w-full max-w-6xl flex gap-8">
           {/* Profile Section */}
-          <div className="flex flex-col items-center flex-grow bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-lg font-bold mb-4">Profile</h2>
-            <div className="w-48 h-48 rounded-full bg-gray-200 flex items-center justify-center mb-4">
-              <Image
-                src={user?.picture || Profile}
-                alt="Profile"
-                className="w-40 h-40 object-cover rounded-full"
-                width={160}
-                height={160}
-              />
-            </div>
-            <h3 className="text-xl font-semibold">{user?.name || 'User'}</h3>
-            <p className="text-gray-500">{user?.email || 'user@example.com'}</p>
-            <div className="mt-6 bg-gray-50 text-black font-bold text-center text-xl rounded-xl py-4 px-8">
-              {savedResults.length} University Searched
+          <div className='flex flex-col flex-grow'>
+            <h2 className="text-2xl font-bold mb-4">Profile</h2>
+            <div className="flex flex-col items-center flex-grow bg-white p-6 rounded-lg shadow-md">
+                <div className="w-48 h-48 rounded-full bg-gray-200 flex items-center justify-center mb-4">
+                <Image
+                    src={user?.picture || Profile}
+                    alt="Profile"
+                    className="w-40 h-40 object-cover rounded-full"
+                    width={160}
+                    height={160}
+                />
+                </div>
+                <h3 className="text-xl font-semibold">{user?.name || 'User'}</h3>
+                <p className="text-gray-500">{user?.email || 'user@example.com'}</p>
+                <div className="mt-6 bg-gray-50 text-black font-bold text-center text-xl rounded-xl py-4 px-8">
+                {savedResults.length} Times Searched
+                </div>
             </div>
           </div>
+        
+          
 
           {/* History Section */}
-          <div className="flex-grow bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-lg font-bold mb-4">History</h2>
-            <div className="max-h-96 overflow-y-auto space-y-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
-              {savedResults.map((item) => (
-                <div
-                  key={item.id}
-                  onClick={() => handleCardClick(item)}
-                  className="flex items-center justify-between p-4 bg-white rounded-lg shadow-sm border hover:shadow-md cursor-pointer"
-                >
-                  <div className="flex items-center gap-4">
-                    <div className="text-2xl text-blue-600">📖</div>
-                    <div>
-                      <h3 className="font-semibold text-gray-700">{item.majorName}</h3>
-                      <p className="text-sm text-gray-500">
-                        {item.universityItems.length} University Recommendation
-                      </p>
-                      <p className="text-sm text-gray-400">{formatDateTime(item.createdAt)}</p>
+          <div className='flex flex-col flex-grow'>
+            <h2 className="text-2xl font-bold mb-4">History</h2>
+            <div className="flex-grow bg-transparent pb-4">
+                
+                <div className="max-h-96 overflow-y-auto space-y-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 pb-8">
+                {savedResults.map((item) => (
+                    <div
+                    key={item.id}
+                    onClick={() => handleCardClick(item)}
+                    className="flex items-center justify-between p-4 pb-3 bg-white rounded-lg shadow-sm border hover:shadow-md cursor-pointer"
+                    >
+                    <div className="flex items-center gap-4">
+                        <div className="text-[3.5rem] text-blue-600">📖</div>
+                        <div>
+                        <h3 className="font-semibold text-gray-700">{item.majorName}</h3>
+                        <p className="text-sm text-gray-500">
+                            {item.universityItems.length} University Recommendation
+                        </p>
+                        <p className="text-xs text-gray-400">{formatDateTime(item.createdAt)}</p>
+                        </div>
                     </div>
-                  </div>
-                  <div className="text-center">
-                    <p className="text-sm text-gray-500">Average Score</p>
-                    <p className="text-2xl font-bold text-gray-800">{item.averageScore}</p>
-                  </div>
+                    <div className="text-center">
+                        <p className="text-sm text-gray-500">Average Score</p>
+                        <p className="text-2xl font-bold text-gray-800">{item.averageScore}</p>
+                    </div>
+                    </div>
+                ))}
                 </div>
-              ))}
             </div>
           </div>
+          
         </div>
       </div>
     </>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,38 +1,38 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/Button';
 import SignOutButton from '../SignOutButton';
-import HomeButton from '../HomeButton';
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const [user, setUser] = useState(null);
 
   // Fetch user session on component mount
   useEffect(() => {
     const fetchUser = async () => {
-        try {
-          const response = await fetch('/api/user', {
-            method: 'GET',
-            credentials: 'include',
-          });
-  
-          if (!response.ok) {
-            router.push('/auth');
-            return;
-          }
-  
-          const userData = await response.json();
-          setUser(userData);
-        } catch (error) {
-          console.error('Error fetching user:', error);
-          router.push('/auth');
+      try {
+        const response = await fetch('/api/user', {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (!response.ok) {
+          setUser(null); // No user session
+          return;
         }
-      };
-  
-      fetchUser();
+
+        const userData = await response.json();
+        setUser(userData);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+        setUser(null); // No user session
+      }
+    };
+
+    fetchUser();
   }, []);
 
   return (
@@ -44,12 +44,30 @@ const Navbar = () => {
         SURE
       </h1>
       <div className="flex gap-6">
-        <HomeButton />
         {user ? (
-          <SignOutButton>Logout</SignOutButton>
+          // Conditional rendering based on the current route
+          <>
+            {pathname === '/home' ? (
+              <Button
+                className="bg-cyan-500 font-medium text-white px-4 py-2 rounded-lg hover:bg-cyan-600"
+                onClick={() => router.push('/profile')}
+              >
+                Profile
+              </Button>
+            ) : pathname === '/profile' ? (
+              <Button
+                className="bg-cyan-500 font-medium text-white px-4 py-2 rounded-lg hover:bg-cyan-600"
+                onClick={() => router.push('/home')}
+              >
+                Home
+              </Button>
+            ) : null}
+            <SignOutButton>Logout</SignOutButton>
+          </>
         ) : (
+          // Show Login Button when no user is logged in
           <Button
-            className="bg-blue-500 font-medium text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+            className="bg-cyan-500 font-medium text-white px-4 py-2 rounded-lg hover:bg-cyan-600"
             onClick={() => router.push('/auth')}
           >
             Login
